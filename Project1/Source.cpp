dd92c028d160b58c2f2ee4896c6decab5fa33f8e
Владислав <@DESKTOP-O03Q1EM>
2024-05-25 18:44:02 +0400

    Добавлено меню игры

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <Windows.h>
 
 // Функция инициализации поля
 char** InitField() {
 	// Переменные ряд, колонна и поле.
 	int row = 10, column = 10;
 	char** field = new char* [row];
-	for (int i = 0; i < column; i++) {
+	for (int i = 0; i < row; i++) {
 		field[i] = new char[column];
 	}
 	// Заполнение полей пустыми ячейками
@@ -56,10 +57,75 @@ void PrintField(char** field) {
 
 int main() {
 	setlocale(LC_ALL, "RU");
-	// Поле игрока и поле противника
-	char** field_player = InitField();
-	char** field_enemy = InitField();
-	PrintField(field_player);
-	PrintField(field_enemy);
-
+	// Переменные для выбора
+	enum{Exit = 0, Play, Options};
+	do {
+		// Переменные настроек игры
+		int SelectInMenu, PlaySet = 0, PlacingSet = 0, BotSet = 0;
+		// Переменные для выбора
+		enum{ExitOfOptions = 0, PlayMode, PlacingSettings, BotSettings};
+		std::cout << "Добро пожаловать в морской бой!";
+		std::cout << "\n1 - Играть\n2 - Настройки\n0 - Выход\n";
+		std::cin >> SelectInMenu;
+		if (SelectInMenu == Exit) {
+			system("cls");
+			std::cout << "Выход...";
+			break;
+		}
+		else if (SelectInMenu == Play) {
+			system("cls");
+			// Поле игрока и поле противника
+			char** field_player = InitField();
+			char** field_enemy = InitField();
+			PrintField(field_player);
+			PrintField(field_enemy);
+		}
+		else if (SelectInMenu == Options) {
+			system("cls");
+			while (true) {
+				int SelectInOptions;
+				std::cout << "Выберите настройки:\n1 - Режим игры\n2 - Настройки размещения кораблей\n3 - Настройки бота\n0 - Выход\n";
+				std::cin >> SelectInOptions;
+				if (SelectInOptions == ExitOfOptions) {
+					system("cls");
+					break;
+				}
+				else if (SelectInOptions == PlayMode) {
+					system("cls");
+					do {
+						std::cout << "Выберите режим игры:\n0 - Против Игрока\n1 - Против Компьютера\n";
+						std::cin >> PlaySet;
+					} while (PlaySet < 0 || PlaySet > 1);
+					system("cls");
+				}
+				else if (SelectInOptions == PlacingSettings) {
+					system("cls");
+					do {
+						std::cout << "Выберите вариант размещения кораблей у игроков:\n0 - Вручную\n1 - Автоматически\n";
+						std::cin >> PlacingSet;
+					} while (PlacingSet < 0 || PlacingSet > 1);
+					system("cls");
+				}
+				else if (SelectInOptions == BotSettings) {
+					system("cls");
+					do {
+						std::cout << "Выберите режим игры для компьютера:\n0 - Случайные выстрелы\n1 - Интеллектуальная игра\n";
+						std::cin >> BotSet;
+					} while (BotSet < 0 || BotSet > 1);
+					system("cls");
+				}
+				else {
+					system("cls");
+					std::cout << "Неверный выбор!\n";
+					continue;
+				}
+			}
+		}
+		else {
+			system("cls");
+			std::cout << "Неверный выбор!\n";
+			continue;
+		}
+		
+	} while (true);
 }
