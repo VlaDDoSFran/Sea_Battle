b324524476b9cfd68ed3ffed3e5a77bca4cbabff
Владислав <@DESKTOP-O03Q1EM>
2024-06-05 19:42:58 +0400

    Добавлены функции размещения кораблей, поворота кораблей. Неполная проверка на возможность поставить корабль на данное поле.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <Windows.h>
+#include <stdlib.h>
+#include "windows.h"
 
 // Функция инициализации поля
 char** InitField() {
@@ -55,42 +56,204 @@ void PrintField(char** field) {
 	std::cout << "\n\n";
 }
 
+void PlaceShip(char** field_player, int type, int x, int y) {
+	if (type == 4) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = 'x';
+		}
+	}
+	else if (type == 3) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = 'x';
+		}
+	}
+	else if (type == 2) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = 'x';
+		}
+	}
+	else if (type == 1) {
+		field_player[y][x] = 'x';
+	}
+	else {
+		return;
+	}
+}
+
+void RotateShip(char** field_player, int type, int x, int y) {
+	int prev_y = y;
+	if (type == 4) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = ' ';
+		}
+		for (int i = 0; i < type; i++) {
+			field_player[x][prev_y++] = 'x';
+		}
+	}
+	else if (type == 3) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = ' ';
+		}
+		for (int i = 0; i < type; i++) {
+			field_player[x][prev_y++] = 'x';
+		}
+	}
+	else if (type == 2) {
+		for (int i = 0; i < type; i++) {
+			field_player[y++][x] = ' ';
+		}
+		for (int i = 0; i < type; i++) {
+			field_player[x][prev_y++] = 'x';
+		}
+	}
+	else {
+		return;
+	}
+}
+
+void PlayerIsPlace(char** field_player, char** field_enemy, int* four_deck, int* three_deck, int* two_deck, int* one_deck, int SetForPlacing) {
+	std::cout << "У вас есть:\n";
+	Sleep(1000);
+	std::cout << "Четырёхпалубные - 1\nТрёхпалубные - 2\nДвухпалубные - 3\nОднопалубные - 4\n";
+	Sleep(1000);
+	std::cout << "Приступайте к расстановке!\n";
+	if (SetForPlacing == 0) {
+		while (*four_deck != 0) {
+			std::cout << "Поставьте 4-палубные корабли:\n";
+			int x, y;
+			char RotateSelect;
+			do {
+				std::cout << "Введите позицию вашего корабля (начало - верхняя точка корабля): \n";
+				std::cin >> x >> y;
+			} while ((x < 0 && x > 9) && (y < 0 && y > 9));
+			PlaceShip(field_player, 4, x, y);
+			PrintField(field_player);
+			std::cout << "Ваш корабль сейчас: |. Желаете повернуть корабль? (r, на взаимно-обратные координаты): ";
+			std::cin >> RotateSelect;
+			if (RotateSelect == 'r' || RotateSelect == 'R') {
+				RotateShip(field_player, 4, x, y);
+			}
+			(*four_deck)--;
+			PrintField(field_player);
+			Sleep(1000);
+		}
+		while (*three_deck != 0) {
+			std::cout << "Поставьте 3-палубные корабли:\n";
+			int x, y;
+			char RotateSelect;
+			do {
+				std::cout << "Введите позицию вашего корабля (начало): \n";
+				std::cin >> x >> y;
+			} while ((x < 0 && x > 9) && (y < 0 && y > 9));
+			PlaceShip(field_player, 3, x, y);
+			PrintField(field_player);
+			std::cout << "Ваш корабль сейчас: |. Желаете повернуть корабль? (r, на взаимно-обратные координаты): ";
+			std::cin >> RotateSelect;
+			if (RotateSelect == 'r' || RotateSelect == 'R') {
+				RotateShip(field_player, 3, x, y);
+			}
+			(*three_deck)--;
+			PrintField(field_player);
+			Sleep(1000);
+		}
+		while (*two_deck != 0) {
+			std::cout << "Поставьте 2-палубные корабли:\n";
+			int x, y;
+			char RotateSelect;
+			do {
+				std::cout << "Введите позицию вашего корабля (начало): \n";
+				std::cin >> x >> y;
+			} while ((x < 0 && x > 9) && (y < 0 && y > 9));
+			PlaceShip(field_player, 2, x, y);
+			PrintField(field_player);
+			std::cout << "Ваш корабль сейчас: |. Желаете повернуть корабль? (r, на взаимно-обратные координаты): ";
+			std::cin >> RotateSelect;
+			if (RotateSelect == 'r' || RotateSelect == 'R') {
+				RotateShip(field_player, 2, x, y);
+			}
+			(*two_deck)--;
+			PrintField(field_player);
+			Sleep(1000);
+		}
+		while (*one_deck != 0) {
+			std::cout << "Поставьте 1-палубные корабли:\n";
+			int x, y;
+			do {
+				std::cout << "Введите позицию вашего корабля (начало): \n";
+				std::cin >> x >> y;
+				if (field_player[y][x] == 'x' ||
+					field_player[y][x - 1] == 'x' ||
+					field_player[y + 1][x - 1] == 'x' ||
+					field_player[y + 1][x] == 'x' ||
+					field_player[y + 1][x + 1] == 'x' ||
+					field_player[y][x + 1] == 'x' ||
+					field_player[y - 1][x + 1] == 'x' ||
+					field_player[y - 1][x] == 'x' ||
+					field_player[y - 1][x - 1] == 'x') {
+					std::cout << "Область, куда вы ставите уже занята кораблём.\n";
+					continue;
+				}
+				else {
+					PlaceShip(field_player, 1, x, y);
+					(*one_deck)--;
+					break;
+				}
+			} while ((x < 0 && x > 9) && (y < 0 && y > 9));
+			PrintField(field_player);
+			Sleep(1000);
+		}
+	}
+}
+
+// Сделать функции по настройкам для бота 
+
 void GoPlay(int SetForPlay, int SetForPlacing, int SetForBot) {
 	// Поле игрока и поле противника
 	char** field_player1 = InitField();
 	char** field_enemy1 = InitField();
 	char** field_player2 = InitField();
 	char** field_enemy2 = InitField();
+	int four_deck1 = 1, three_deck1 = 2, two_deck1 = 3, one_deck1 = 4;
+	int four_deck2 = 1, three_deck2 = 2, two_deck2 = 3, one_deck2 = 4;
 	int turn = 1, i_ready;
-	while (true) {
+	do {
 		while (turn == 1) {
-			std::cout << "Очередь " << turn << " игрока.\nВы готовы?\n(Любое число, если да):\n";
+			std::cout << "Очередь " << turn << " игрока.\nВы готовы?\n(Любое число, если да | 0 - нет, в меню):\n";
 			std::cin >> i_ready;
+			Sleep(1000);
 			system("cls");
 			PrintField(field_player1);
 			PrintField(field_enemy1);
+			PlayerIsPlace(field_player1, field_enemy1, &four_deck1, &three_deck1, &two_deck1, &one_deck1, SetForPlacing);
 			system("cls");
 			turn = 2;
 		}
 		while (turn == 2) {
-			std::cout << "Очередь " << turn << " игрока.\nВы готовы?\n(Любое число, если да):\n";
-			std::cin >> i_ready;
-			system("cls");
-			PrintField(field_player2);
-			PrintField(field_enemy2);
-			system("cls");
-			turn = 1;
+			if (SetForPlay == 0) {
+				std::cout << "Очередь " << turn << " игрока.\nВы готовы?\n(Любое число, если да | 0 - нет, в меню):\n";
+				std::cin >> i_ready;
+				Sleep(1000);
+				system("cls");
+				PrintField(field_player2);
+				PrintField(field_enemy2);
+				PlayerIsPlace(field_player2, field_enemy2, &four_deck2, &three_deck2, &two_deck2, &one_deck2, SetForPlacing);
+				system("cls");
+				turn = 1;
+			}
+			else if (SetForPlay == 1) {
+				std::cout << "Очередь бота.\n";
+			}
 		}
-	}
+	} while (i_ready != 0);
 }
 
 int main() {
 	setlocale(LC_ALL, "RU");
 	// Переменные для выбора
 	enum{Exit = 0, Play, Options};
+	// Переменные настроек игры
+	int SelectInMenu, PlaySet = 0, PlacingSet = 0, BotSet = 0;
 	do {
-		// Переменные настроек игры
-		int SelectInMenu, PlaySet = 0, PlacingSet = 0, BotSet = 0;
 		// Переменные для выбора
 		enum{ExitOfOptions = 0, PlayMode, PlacingSettings, BotSettings};
 		std::cout << "Добро пожаловать в морской бой!";
